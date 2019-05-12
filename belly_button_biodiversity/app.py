28f23a96d173648e98a31fd7b503e6d16d428e05
Olesya Bondarenko <ovbondarenko@gmail.com>
2019-05-11 21:06:51 -0700

    Minor changes

--- belly_button_biodiversity/app.py
+++ belly_button_biodiversity/app.py
@@ -102,10 +102,10 @@ if __name__ == "__main__":
     app.run()
 
 # prevent cached responses
-# if app.config["DEBUG"]:
-#     @app.after_request
-#     def after_request(response):
-#         response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-#         response.headers["Expires"] = 0
-#         response.headers["Pragma"] = "no-cache"
-#         return response
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
